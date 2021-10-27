da9c2ccaf58d2cdacb82023cf0f802cfc3e112a3
GradedJestRisk <graded.jest.risk@gmail.com>
2021-10-27 15:10:19 +0200

    Add flakier tests

--- api/tests/flaky.test.js
+++ api/tests/flaky.test.js
@@ -1,8 +1,26 @@
 const assert = require('assert');
 
+const randomBetween = (min, max)=>{
+   return Math.floor(
+      Math.random() * (max - min) + min
+   )
+}
+
 describe('flaky', function () {
-   it('should fail when seconds are even', function () {
+
+   it('should fail sometimes, when an random number is not in a range', function () {
+      const number = randomBetween(0,100);
+      assert.strictEqual((number > 25), false);
+   });
+
+   it('should fail regularly, when seconds are even', function () {
       const seconds = new Date().getSeconds();
       assert.strictEqual(((seconds % 2) === 0), false);
    });
+
+   it('should fail most of the time, when an random number is not in a range', function () {
+      const number = randomBetween(0,100);
+      assert.strictEqual((number > 75), false);
+   });
+
 });
